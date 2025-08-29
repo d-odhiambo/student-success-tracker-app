64b6eb1332698863541936cb5106aaf0ca9bbc5e
zophar <zophar.nyakundi@student.moringaschool.com>
2025-08-29 13:33:21 +0300

    Add state for inputs

--- src/Components/Students/AddStudentForm.jsx
+++ src/Components/Students/AddStudentForm.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function AddStudentForm() {
+  const [name, setName] = useState("");
+  const [grade, setGrade] = useState("");
+
   return (
     <form>
       <h3>Add Student</h3>
-      <input type="text" placeholder="Student Name" />
-      <input type="text" placeholder="Grade" />
+      <input
+        type="text"
+        placeholder="Student Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Grade"
+        value={grade}
+        onChange={(e) => setGrade(e.target.value)}
+      />
       <button type="submit">Add</button>
     </form>
   );
